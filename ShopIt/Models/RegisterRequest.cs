331f6dcbca403d08f67c5cc5eda000fbebe3d60a
ryzzzin <rysinb@yandex.ru>
2021-07-22 21:55:10 +0600

    [add] new request models

---
+++ ShopIt/Models/RegisterRequest.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShopIt.Models
+{
+    public class RegisterRequest
+    {
+        [Required]
+        [Display(Name = "UserName")]
+        [DataType(DataType.Text)]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Password not same")]
+        [Display(Name = "Confirm the password")]
+        [DataType(DataType.Password)]
+        public string PasswordConfirmation { get; set; }
+    }
+}
