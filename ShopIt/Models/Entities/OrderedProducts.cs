e594de1a42a50d5a3fae98206e844ce9cf8643c9
ryzzzin <rysinb@yandex.ru>
2021-07-23 19:13:18 +0600

    [add] OrderedProducts

---
+++ ShopIt/Models/Entities/OrderedProducts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopIt.Models.Entities
+{
+    public class OrderedProducts : BaseEntity<Guid>
+    {
+
+        public Guid OrderId { get; set; }
+        [ForeignKey(nameof(OrderId))]
+        public virtual ApplicationUser Order { get; set; }
+        public Guid ProductId { get; set; }
+        [ForeignKey(nameof(ProductId))]
+        public virtual Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
