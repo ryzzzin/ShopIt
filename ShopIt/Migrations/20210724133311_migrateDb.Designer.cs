b3d7467beac1f344d0a275cdd77806eaef13050b
ryzzzin <rysinb@yandex.ru>
2021-07-25 00:37:07 +0600

    [add] new migration

--- ShopIt/Migrations/20210724133311_migrateDb.Designer.cs
+++ ShopIt/Migrations/20210724133311_migrateDb.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ShopIt.Contexts;
 
 namespace ShopIt.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210724133311_migrateDb")]
+    partial class migrateDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
