200b4be77a2320bce932a86546d581a8210399fa
ryzzzin <rysinb@yandex.ru>
2021-07-25 00:48:42 +0600

    [add] EditAddedProductRequest

---
+++ ShopIt/Models/AdminModels/EditAddedProductRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopIt.Models.AdminModels
+{
+    public class EditAddedProductRequest
+    {
+        [Required(ErrorMessage = "Id field is required")]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "ProductId field is required")]
+        public Guid ProductId { get; set; }
+        [Required(ErrorMessage = "Quantity field is required")]
+        public int Quantity { get; set; }
+    }
+}
