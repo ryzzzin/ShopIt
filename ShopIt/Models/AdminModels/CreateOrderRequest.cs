bb490189fdb72ec3e808a288ca62c061fc97c1cc
ryzzzin <rysinb@yandex.ru>
2021-07-24 17:27:25 +0600

    [add] CreateOrderRequest

---
+++ ShopIt/Models/AdminModels/CreateOrderRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopIt.Models.AdminModels
+{
+    public class CreateOrderRequest
+    {
+        public Guid UserId { get; set; }
+        public int StatusId { get; set; }
+    }
+}
