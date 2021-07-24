2a891d692e947b367bba113c7225dcd490cab7df
ryzzzin <rysinb@yandex.ru>
2021-07-24 17:27:38 +0600

    [add] CreateStatusRequest

---
+++ ShopIt/Models/AdminModels/CreateStatusRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShopIt.Models.AdminModels
+{
+    public class CreateStatusRequest
+    {
+        [Required(ErrorMessage = "Title field is required")]
+        public string Title { get; set; }
+        public string Icon { get; set; }
+    }
+}
