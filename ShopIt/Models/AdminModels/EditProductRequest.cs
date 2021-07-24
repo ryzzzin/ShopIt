f4ed2fe79f93322fb42e48704427fdd18d94840d
ryzzzin <rysinb@yandex.ru>
2021-07-25 00:50:52 +0600

    [fix] ProductId, CategoryId data types

--- ShopIt/Models/AdminModels/EditProductRequest.cs
+++ ShopIt/Models/AdminModels/EditProductRequest.cs
@@ -9,15 +9,15 @@ namespace ShopIt.Models.AdminModels
     public class EditProductRequest
     {
         [Required(ErrorMessage = "Id field is required")]
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         [Required(ErrorMessage = "Title field is required")]
         public string Title { get; set; }
         [Required(ErrorMessage = "Price field is required")]
         public decimal Price { get; set; }
-        [Required(ErrorMessage = "Quantity field is required")]
-        public int Quantity { get; set; }
+        [Required(ErrorMessage = "Stock field is required")]
+        public int Stock { get; set; }
         [Required(ErrorMessage = "CategoryId field is required")]
-        public Guid CategoryId { get; set; }
+        public int CategoryId { get; set; }
         [Required(ErrorMessage = "Description field is required")]
         public string Description { get; set; }
     }
