cd312b03455f0799d1c6dfc84f8723395495d4e7
ryzzzin <rysinb@yandex.ru>
2021-07-24 17:29:19 +0600

    [add] EditStatusRequest

---
+++ ShopIt/Models/AdminModels/EditStatusRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace ShopIt.Models.AdminModels
+{
+    public class EditStatusRequest
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Icon { get; set; }
+    }
+}
